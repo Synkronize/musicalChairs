af7887747fca47e27772c5d4e99a4d489ae2da33
Synkronize <judegithub@gmail.com>
2017-12-06 21:43:02 -0500

    Window now has a list element to it

--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,45 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Jude on 12/6/2017.
  */
 public class Player{
     Player(){
+
+            //get resource grabs things, with get resource we can grab things that are sitting in side the class folder.
+            File directory = new File("C:\\Users\\Jude\\Documents\\javaSoundPractice\\src");
+            List<String> list = Arrays.asList(directory.list(
+                    new FilenameFilter() {
+                        @Override
+                        public boolean accept(File dir, String name) {
+                            return name.endsWith(".wav");
+                        }
+                    }
+            ));
+        String[] songs = new String[list.size()];
+        songs = list.toArray(songs);
+        JList<String> playList = new JList(songs);
+        playList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        playList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        playList.setVisibleRowCount(-1);
         JFrame frame = new JFrame();
-        JPanel pane = new JPanel();
+        JPanel panelTop = new JPanel();
+        JPanel panelBottom = new JPanel();
+        panelBottom.setLayout(new BoxLayout(panelBottom,BoxLayout.X_AXIS));
         JButton play = new JButton();
         JButton pause = new JButton();
+        JScrollPane scrollPane = new JScrollPane(playList);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        panelTop.add(scrollPane);
+
         try{
             Image img = ImageIO.read(getClass().getResource("Play24.gif"));
             Image pauseImg = ImageIO.read(getClass().getResource("Pause16.gif"));
@@ -32,12 +60,12 @@ public class Player{
 
 
 
-        play.setSize(1,10);
-        frame.getContentPane().add(pane);
-        pane.add(play);
-        pane.add(pause);
+        panelBottom.add(play);
+        panelBottom.add(pause);
+        frame.getContentPane().add(BorderLayout.WEST,panelTop);
+        frame.getContentPane().add(BorderLayout.SOUTH,panelBottom);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
+        frame.setSize(300,250);
         frame.setVisible(true);
     }
     public static void main(String[] args){
